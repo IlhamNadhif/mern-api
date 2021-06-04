6cb1ee2cf2c3c6aa2e3d43510edc7b7a8ed0e58c
Ilham Nadhif <ilham.10rpl2.2019@gmail.com>
2021-06-04 09:15:09 +0700

    update

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const multer = require('multer')
-const path = require('path')
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const path = require("path");
 
-const app = express()
-const authRoutes = require('./src/routes/auth')
-const blogRoutes = require('./src/routes/blog')
-const port = 8002
+const app = express();
 
-mongoose.connect('mongodb://localhost/mern-blog', {useNewUrlParser: true, useUnifiedTopology: true});
+const authRoutes = require("./src/routes/auth");
+const blogRoutes = require("./src/routes/blog");
+
+mongoose.connect("mongodb://localhost/mern-blog", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 const db = mongoose.connection;
 
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
-app.use('/images', express.static(path.join(__dirname, 'images')))
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-});
-
-app.use('/v1/auth', authRoutes);
-app.use('/v1/blog', blogRoutes);
+app.use("/v1/auth", authRoutes);
+app.use("/v1/blog", blogRoutes);
 
 app.use((error, req, res, next) => {
-    const status = error.errorStatus || 500;
-    const message = error.message;
-    const data = error.data;
-    res.status(status).json({
-        message: message,
-        data: data
-    })
-})
+  const status = error.errorStatus || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({
+    message: message,
+    data: data,
+  });
+});
 
+const port = 8000;
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
--- src/controllers/blog.js
+++ src/controllers/blog.js
@@ -1,187 +1,185 @@
-const {validationResult} = require('express-validator')
-const multer = require('multer')
-const path = require('path')
-const fs = require('fs')
-const BlogPost = require('../models/blog')
+const { validationResult } = require("express-validator");
+const multer = require("multer");
+const path = require("path");
+const fs = require("fs");
+const BlogPost = require("../models/blog");
 
 const storage = multer.diskStorage({
-    destination: function (request, file, callback) {
-        callback(null, './images')
-    },
+  destination: function (request, file, callback) {
+    callback(null, "./images");
+  },
 
-    filename: function (request, file, callback) {
-        callback(null, Date.now() + '-' + file.originalname)
-    },
-})
+  filename: function (request, file, callback) {
+    callback(null, Date.now() + "-" + file.originalname);
+  },
+});
 
 const upload = multer({
-    storage: storage
-})
+  storage: storage,
+});
 
 const createBLogPost = (req, res, next) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        const err = new Error('Input value tidak sesuai')
-        err.errorStatus = 400;
-        err.data = errors.array();
-        throw err;
-    }
-    if (!req.file) {
-        const err = new Error('Image harus di upload')
-        err.errorStatus = 422;
-        throw err;
-    }
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Input value tidak sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+  if (!req.file) {
+    const err = new Error("Image harus di upload");
+    err.errorStatus = 422;
+    throw err;
+  }
 
-    const title = req.body.title;
-    const image = req.file.filename
-    const body = req.body.body;
+  const title = req.body.title;
+  const image = req.file.filename;
+  const body = req.body.body;
 
-    const Posting = new BlogPost({
-        title: title,
-        body: body,
-        image: image,
-        author: {
-            uid: 1,
-            name: 'Ilham'
-        }
+  const Posting = new BlogPost({
+    title: title,
+    body: body,
+    image: image,
+    author: {
+      uid: 1,
+      name: "Ilham",
+    },
+  });
+  Posting.save()
+    .then((result) => {
+      res.status(201).json({
+        message: "Create blog succes",
+        data: result,
+      });
     })
-    Posting.save()
-        .then((result) => {
-            res.status(201).json({
-                message: 'Create blog succes',
-                data: result
-            })
-        })
-        .catch(err => {
-            console.log(err)
-        })
-
-}
+    .catch((err) => {
+      console.log(err);
+    });
+};
 
 const getAllBlogPost = (req, res, next) => {
-    const currentPage = req.query.page || 1
-    const perPage = req.query.perPage || 5
-    let totalItems;
+  const currentPage = req.query.page || 1;
+  const perPage = req.query.perPage || 5;
+  let totalItems;
 
-    BlogPost.find()
-        .countDocuments()
-        .then((count) => {
-            totalItems = count
-            return BlogPost.find()
-                .skip((parseInt(currentPage) - 1) * parseInt(perPage))
-                .limit(parseInt(perPage))
-        })
-        .then((result) => {
-            res.status(200).json({
-                message: 'Data blog post berhasil dipanggil',
-                data: result,
-                total_data: totalItems,
-                per_page: parseInt(perPage),
-                current_page: parseInt(currentPage)
-            })
-        })
-        .catch((err) => {
-            next(err)
-        })
-}
+  BlogPost.find()
+    .countDocuments()
+    .then((count) => {
+      totalItems = count;
+      return BlogPost.find()
+        .skip((parseInt(currentPage) - 1) * parseInt(perPage))
+        .limit(parseInt(perPage));
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Data blog post berhasil dipanggil",
+        data: result,
+        total_data: totalItems,
+        per_page: parseInt(perPage),
+        current_page: parseInt(currentPage),
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 const getBlogPostById = (req, res, next) => {
-    const postId = req.params.postId
-    BlogPost.findById(postId)
-        .then((result) => {
-            if (!result) {
-                const error = new Error('BlogPost tidak ditemukan')
-                error.errorStatus = 404
-                throw error
-            }
-            res.status(200).json({
-                message: 'Data blog post berhasil dipanggil',
-                data: result
-            })
-        })
-        .catch(err => {
-            next(err)
-        })
-}
+  const postId = req.params.postId;
+  BlogPost.findById(postId)
+    .then((result) => {
+      if (!result) {
+        const error = new Error("BlogPost tidak ditemukan");
+        error.errorStatus = 404;
+        throw error;
+      }
+      res.status(200).json({
+        message: "Data blog post berhasil dipanggil",
+        data: result,
+      });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
 
 const updateBlogPost = (req, res, next) => {
-    const errors = validationResult(req)
-    if (!errors.isEmpty()) {
-        const err = new Error('Input value tidak sesuai')
-        err.errorStatus = 400;
-        err.data = errors.array();
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const err = new Error("Input value tidak sesuai");
+    err.errorStatus = 400;
+    err.data = errors.array();
+    throw err;
+  }
+  if (!req.file) {
+    const err = new Error("Image harus di upload");
+    err.errorStatus = 422;
+    throw err;
+  }
+
+  const title = req.body.title;
+  const image = req.file.filename;
+  const body = req.body.body;
+  const postId = req.params.postId;
+
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("BlogPost tidak ditemukan");
+        err.errorStatus(404);
         throw err;
-    }
-    if (!req.file) {
-        const err = new Error('Image harus di upload')
-        err.errorStatus = 422;
-        throw err;
-    }
+      }
+      post.title = title;
+      post.body = body;
+      post.image = image;
 
-    const title = req.body.title;
-    const image = req.file.filename
-    const body = req.body.body;
-    const postId = req.params.postId
-
-    BlogPost.findById(postId)
-        .then((post) => {
-            if (!post) {
-                const err = new Error('BlogPost tidak ditemukan')
-                err.errorStatus(404)
-                throw err
-            }
-            post.title = title
-            post.body = body
-            post.image = image
-
-            return post.save()
-        })
-        .then((result) => {
-            res.status(200).json({
-                message: 'Update Succes',
-                data: result
-            })
-        })
-        .catch(err => next(err))
-}
+      return post.save();
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Update Succes",
+        data: result,
+      });
+    })
+    .catch((err) => next(err));
+};
 
 const deleteBlogPost = (req, res, next) => {
-    const postId = req.params.postId
+  const postId = req.params.postId;
 
-    BlogPost.findById(postId)
-        .then(post => {
-            if (!post) {
-                const err = new Error('BlogPost tidak ditemukan')
-                err.errorStatus(404)
-                throw err
-            }
+  BlogPost.findById(postId)
+    .then((post) => {
+      if (!post) {
+        const err = new Error("BlogPost tidak ditemukan");
+        err.errorStatus(404);
+        throw err;
+      }
 
-            removeImage(post.image)
-            return BlogPost.findByIdAndRemove(postId)
-
-        })
-        .then((result) => {
-            res.status(200).json({
-                message: 'Hapus Blog Post berhasil',
-                data: result,
-            })
-        })
-        .catch(err => next(err))
-}
+      removeImage(post.image);
+      return BlogPost.findByIdAndRemove(postId);
+    })
+    .then((result) => {
+      res.status(200).json({
+        message: "Hapus Blog Post berhasil",
+        data: result,
+      });
+    })
+    .catch((err) => next(err));
+};
 
 const removeImage = (filePath) => {
-    // console.log('filePath', filePath)
-    // console.log('__dirname', __dirname)
-    filePath = path.join(__dirname, '../../images/', filePath)
-    console.log(filePath)
-    fs.unlink(filePath, err => console.log(err))
-}
+  // console.log('filePath', filePath)
+  // console.log('__dirname', __dirname)
+  filePath = path.join(__dirname, "../../images/", filePath);
+  console.log(filePath);
+  fs.unlink(filePath, (err) => console.log(err));
+};
 
 module.exports = {
-    createBLogPost,
-    upload,
-    getAllBlogPost,
-    getBlogPostById,
-    updateBlogPost,
-    deleteBlogPost,
-}
+  createBLogPost,
+  upload,
+  getAllBlogPost,
+  getBlogPostById,
+  updateBlogPost,
+  deleteBlogPost,
+};
